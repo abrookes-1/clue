2caaa22c7aaad091559dd15dd4bf0c349102dfc2
Aidan Brookes <aaabrookes@gmail.com>
2019-02-28 14:26:44 -0700

    added empty test functions

---
+++ src/tests/IntBoardTests.java
@@ -0,0 +1,52 @@
+package tests;
+import org.junit.*;
+
+public class IntBoardTests {
+	@Before
+	public void setup() {
+		board = new IntBoard();  // constructor should call calcAdjacencies() so you can test them
+	}
+	
+	// tests for four corners of the board
+	@Test
+	public void testAdjacencyTL() {
+		// TL = top left
+		
+	}
+	
+	@Test
+	public void testAdjacencyTR() {
+		// TL = top right
+		
+	}
+	
+	@Test
+	public void testAdjacencyBL() {
+		// TL = bottom left
+		
+	}
+	
+	@Test
+	public void testAdjacencyBR() {
+		// BR = bottom right
+		
+	}
+	
+	@Test
+	public void testAdjacency11() {
+		
+		
+	}
+	
+	@Test
+	public void testAdjacency22() {
+		
+		
+	}
+	
+	@Test
+	public void testTargets() {
+		
+		
+	}
+}
