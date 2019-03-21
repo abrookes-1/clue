007d2d74cb33570752e0207e1f56383f86bbc065
Aidan Brookes <aaabrookes@gmail.com>
2019-03-21 15:17:23 -0600

    added barebones classes relating to players

---
+++ src/clueGame/Card.java
@@ -0,0 +1,9 @@
+package clueGame;
+
+public class Card {
+	private String cardName;
+	
+	public Boolean equals() {
+		return null;
+	}
+}
---
+++ src/clueGame/Player.java
@@ -0,0 +1,13 @@
+package clueGame;
+import java.awt.Color;
+
+public class Player {
+	String playerName;
+	int row;
+	int column;
+	Color color;
+	
+	public Card disproveSuggestion(Solution suggestion) {
+		return null;
+	}
+}
