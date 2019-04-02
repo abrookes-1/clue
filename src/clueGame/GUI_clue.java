f5d49fda13696329a7c95ccd281580dd1e97cf73
Aidan Brookes <aaabrookes@gmail.com>
2019-04-02 14:13:52 -0600

    added code for guid window (empty)

--- src/clueGame/GUI_clue.java
+++ src/clueGame/GUI_clue.java
@@ -11,6 +11,19 @@ import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
-public class GUI_clue {
+public class GUI_clue extends JPanel{
 	
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Clue");
+		frame.setSize(250, 150);
+		
+		GUI_clue gui_clue = new GUI_clue();
+		frame.add(gui_clue, BorderLayout.CENTER);
+		
+		frame.setVisible(true);
+	}
 }
