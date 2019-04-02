14727e01aa064f9d16e89adf378f06039e4ddf3b
Aidan Brookes <aaabrookes@gmail.com>
2019-04-02 14:08:28 -0600

    Create GUI_clue.java

---
+++ src/clueGame/GUI_clue.java
@@ -0,0 +1,16 @@
+package clueGame;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+public class GUI_clue {
+	
+}
