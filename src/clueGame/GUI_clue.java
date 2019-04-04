65252828815ed6fdbfb3230b695575453928e646
Aidan Brookes <aaabrookes@gmail.com>
2019-04-04 14:16:54 -0600

    Update GUI_clue.java

--- src/clueGame/GUI_clue.java
+++ src/clueGame/GUI_clue.java
@@ -12,18 +12,32 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
 public class GUI_clue extends JPanel{
+	 private JPanel createNamePanel() {
+		 	JPanel panel = new JPanel();
+		 	// Use a grid layout, 1 row, 2 elements (label, text)
+			panel.setLayout(new GridLayout(1,1));
+//		 	JLabel nameLabel = new JLabel("Name");
+//			name = new JTextField(20);
+//			panel.add(nameLabel);
+//			panel.add(name);
+			panel.setBorder(new TitledBorder (new EtchedBorder(), "Who are you?"));
+			return panel;
+	}
 	
-	public GUI_clue() {
-//		setLayout(new GridLayout(2,0));
-//		JPanel panel = createNamePanel();
-//		add(panel);
+	public GUI_clue(int rows, int cols) {
+		setLayout(new GridLayout(rows, cols));
+		JPanel panel = createNamePanel();
+		add(panel);
 //		panel = createButtonPanel();
 //		add(panel);
 	}
 	
 	public static void main(String[] args) {
-		//static Board gameBoard = Board.getInstance();
-		
+		Board gameBoard = Board.getInstance();
+		gameBoard.setConfigFiles("ClueMap.csv", "RoomKey.txt", "players.txt", "weapons.txt");
+		gameBoard.initialize();
+		int rows = gameBoard.getNumRows();
+		int cols = gameBoard.getNumColumns();
 		
 		// begin gui functions
 		JFrame frame = new JFrame();
@@ -32,7 +46,7 @@ public class GUI_clue extends JPanel{
 		frame.setTitle("Clue");
 		frame.setSize(250, 150);
 		
-		GUI_clue gui_clue = new GUI_clue();
+		GUI_clue gui_clue = new GUI_clue(rows, cols);
 		frame.add(gui_clue, BorderLayout.CENTER);
 		
 		frame.setVisible(true);
